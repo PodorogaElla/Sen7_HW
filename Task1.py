2a8d7c7a21264fb9b402d9a3d02022431f65e247
PodorogaElla <Elka_forest@mail.ru>
2023-06-16 18:40:42 +0500

    added Task2

--- Task1.py
+++ Task1.py
@@ -3,13 +3,16 @@ import scipy.stats as stats
 import matplotlib.pyplot as plt
 
 alpha = 0.05
-x= np.array([380, 420, 290])
-y= np.array([140, 360, 200, 900])
-d = stats.mannwhitneyu(x,y)
+x = np.array([380, 420, 290])
+y = np.array([140, 360, 200, 900])
+d = stats.mannwhitneyu(x, y)
 print('\n>>> Statistics = %.2f \n>>> Р-value = %.3f' % (d))
-s, p = stats.mannwhitneyu(x,y)
+s, p = stats.mannwhitneyu(x, y)
 if p > alpha:
-    print('>>> Cтатистически значимых различий нет, нулевая гипотеза не отвергается.')
+    print(
+        '>>> Cтатистически значимых различий нет, нулевая гипотеза '
+        'не отвергается.')
 else:
     print(
-        '>>> Cтатистически значимые различия присутствуют, нулевую гипотезу отвергаем.')
+        '>>> Cтатистически значимые различия присутствуют, нулевую '
+        'гипотезу отвергаем.')
